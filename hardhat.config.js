1f5163e54488f6ee7bf656dcd15c356ce4578bf3
M.Siddique <108999811+ABossOfMyself@users.noreply.github.com>
2022-08-27 21:19:37 +0500

    Updated hardhat.config.js
    Added few things that were missing.

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,32 +1,54 @@
 require("@nomicfoundation/hardhat-toolbox")
 require("hardhat-deploy")
-require("dotenv").config()
+
 require("@nomiclabs/hardhat-ethers")
 const { getUsedIdentifiers } = require("typechain")
 require("@nomicfoundation/hardhat-toolbox")
+require("hardhat-gas-reporter")
+require("dotenv").config()
 // require("@nomiclabs/hardhat-waffle")
 // require('hardhat/config').HardhatUserConfig
 
-// require("dotenv").config()
-// require("hardhat-gas-reporter")
 // require("@nomiclabs/hardhat-etherscan")
 // require("solidity-coverage")
 
 //  @type import('hardhat/config').HardhatUserConfig
 
+const RINKEBY_RPC_URL = process.env.RINKEBY_RPC_URL
+const PRIVATE_KEY = process.env.PRIVATE_KEY
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY
+const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY
+
 module.exports = {
     solidity: {
         compilers: [{ version: "0.8.8" }, { version: "0.6.6" }],
     },
-
-        defaultNetwork: "hardhat",
-
-        namedAccounts: {
-            deployer: {
-                default: 0,
-            },
-            users: {
-                default: 1,
-            },
+    defaultNetwork: "hardhat",
+    networks: {
+        rinkeby: {
+            url: RINKEBY_RPC_URL,
+            accounts: [PRIVATE_KEY],
+            chainId: 4,
+            blockConfirmations: 6,
         },
+    },
+    gasReporter: {
+        enabled: true,
+        outputFile: "gas-report.txt",
+        noColors: true,
+        currency: "USD",
+        coinmarketcap: COINMARKETCAP_API_KEY,
+        token: "ETH",
+    },
+    etherscan: {
+        apiKey: ETHERSCAN_API_KEY,
+    },
+    namedAccounts: {
+        deployer: {
+            default: 0,
+        },
+        player: {
+            default: 1,
+        },
+    },
 }
